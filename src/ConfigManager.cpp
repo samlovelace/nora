e229694d11895cbe2f2263bd825deb2b94a168e4
samlovelace <samlovelace12@gmail.com>
2025-03-01 12:49:34 -0500

    updates config file parsing and logging

--- src/ConfigManager.cpp
+++ src/ConfigManager.cpp
@@ -18,19 +18,16 @@ void ConfigManager::loadConfig(const std::string& filename)
     try {
         YAML::Node yamlConfig = YAML::LoadFile(filename);
 
+        std::stringstream s;
+        s << "Configuration: \n"; 
+        s << YAML::Dump(yamlConfig);
+        LOGD << s.str();  
+
         // Parse Robot Info
         mConfig.robot_name = yamlConfig["robot"]["name"].as<std::string>();
         mConfig.robot_ip = yamlConfig["robot"]["ip_address"].as<std::string>();
         mConfig.rigid_body_id = yamlConfig["robot"]["rigid_body_id"].as<int>();
         mConfig.mStateTrackerConfig.mNetwork.Local.IP = mConfig.robot_ip; 
-
-        // Parse State Vector
-        mConfig.position = yamlConfig["state_vector"]["position"].as<bool>();
-        mConfig.velocity = yamlConfig["state_vector"]["velocity"].as<bool>();
-        mConfig.acceleration = yamlConfig["state_vector"]["acceleration"].as<bool>();
-        mConfig.orientation = yamlConfig["state_vector"]["orientation"].as<bool>();
-        mConfig.angular_velocity = yamlConfig["state_vector"]["angular_velocity"].as<bool>();
-        mConfig.angular_acceleration = yamlConfig["state_vector"]["angular_acceleration"].as<bool>();
 
         // Parse ROS Topics
         mConfig.state_topic = yamlConfig["topics"]["state"].as<std::string>();
@@ -54,43 +51,7 @@ void ConfigManager::loadConfig(const std::string& filename)
         mConfig.mStateTrackerConfig.mNetwork.Server.CmdPort = mConfig.mStateTrackerConfig.mNetwork.Multicast.CmdPort;
         mConfig.mStateTrackerConfig.mNetwork.Server.DataPort = mConfig.mStateTrackerConfig.mNetwork.Multicast.DataPort;
 
-        printConfig();
     } catch (const YAML::Exception& e) {
         LOGE << "[ERROR] Failed to load YAML: " << e.what();
     }
-}
-
-void ConfigManager::printConfig() const {
-    LOGD << "===== Configuration Loaded =====";
-    LOGD << "Robot Name: " << mConfig.robot_name;
-    LOGD << "Robot IP: " << mConfig.robot_ip;
-    LOGD << "Rigid Body ID: " << mConfig.rigid_body_id;
-    LOGD << "--------------------------------";
-    LOGD << "State Vector Configuration:";
-    LOGD << "  Position: " << (mConfig.position ? "Enabled" : "Disabled");
-    LOGD << "  Velocity: " << (mConfig.velocity ? "Enabled" : "Disabled");
-    LOGD << "  Acceleration: " << (mConfig.acceleration ? "Enabled" : "Disabled");
-    LOGD << "  Orientation: " << (mConfig.orientation ? "Enabled" : "Disabled");
-    LOGD << "  Angular Velocity: " << (mConfig.angular_velocity ? "Enabled" : "Disabled");
-    LOGD << "  Angular Acceleration: " << (mConfig.angular_acceleration ? "Enabled" : "Disabled");
-    LOGD << "--------------------------------";
-    LOGD << "ROS Topics:";
-    LOGD << "  State Topic: " << mConfig.state_topic;
-    LOGD << "--------------------------------";
-    LOGD << "Motion Capture System: " << mConfig.mStateTrackerConfig.mInterface;
-    LOGD << "Update Rate: " << mConfig.mStateTrackerConfig.mUpdateRate << " Hz";
-    LOGD << "--------------------------------";
-    LOGD << "Network Configuration:";
-    if (mConfig.mStateTrackerConfig.mInterface == "optitrack") {
-        LOGD << "  OptiTrack:";
-        LOGD << "    Server Address: " << mConfig.mStateTrackerConfig.mNetwork.Server.IP; 
-        LOGD << "    Multicast Address: " << mConfig.mStateTrackerConfig.mNetwork.Multicast.IP;
-        LOGD << "    Command Port: " << mConfig.mStateTrackerConfig.mNetwork.Multicast.CmdPort;
-        LOGD << "    Data Port: " << mConfig.mStateTrackerConfig.mNetwork.Multicast.DataPort;
-    } else if (mConfig.mStateTrackerConfig.mInterface == "vicon") {
-        LOGD << "  Vicon:";
-        LOGD << "    Server Address: " << mConfig.mStateTrackerConfig.mNetwork.Multicast.IP;
-        LOGD << "    Port: " << mConfig.mStateTrackerConfig.mNetwork.Multicast.DataPort;
-    }
-    LOGD << "================================";
 }
